aefa063ded2711a2ec7bc8e7886b329c4cae42d4
azak03 <97641168+azak03@users.noreply.github.com>
2025-03-02 21:36:14 -0800

    Update contact.js

--- resources/contact.js
+++ resources/contact.js
@@ -1,55 +1,57 @@
-let form = document.getElementById("contactform");
-let comments = document.getElementById("comments");
-let counter = document.getElementById("counter");
-let formerrors = {};
-formerrors['name'] = [];
-formerrors['email'] = [];
-formerrors['comments'] = [];
-let erroroutput = document.getElementsByClassName("error-message")[0];  
-document.querySelectorAll("input[pattern]").forEach(field => {
-let pattern = new RegExp(field.pattern);
-field.addEventListener("input", (e) => {
-    if (!pattern.test(e.target.value) && e.target.value.length > 0) {
-        formerrors[field.name].push("Invalid character!");
-        e.target.classList.add("flash");
-        erroroutput.textContent = `Rejected character in ${field.name} field!`;
-        erroroutput.classList.add("fade");
-        setTimeout(() => {
-            e.target.classList.remove("flash");
-            erroroutput.classList.remove("fade");
-        }, 1000);
-        e.target.value = e.target.value.slice(0, -1);
-    }
+$(document).ready(function() {
+    let form = document.getElementById("contactform");
+    let comments = document.getElementById("comments");
+    let counter = document.getElementById("counter");
+    let formerrors = {};
+    formerrors['name'] = [];
+    formerrors['email'] = [];
+    formerrors['comments'] = [];
+    let erroroutput = document.getElementsByClassName("error-message")[0];  
+    document.querySelectorAll("input[pattern]").forEach(field => {
+    let pattern = new RegExp(field.pattern);
+    field.addEventListener("input", (e) => {
+        if (!pattern.test(e.target.value) && e.target.value.length > 0) {
+            formerrors[field.name].push("Invalid character!");
+            e.target.classList.add("flash");
+            erroroutput.textContent = `Rejected character in ${field.name} field!`;
+            erroroutput.classList.add("fade");
+            setTimeout(() => {
+                e.target.classList.remove("flash");
+                erroroutput.classList.remove("fade");
+            }, 1000);
+            e.target.value = e.target.value.slice(0, -1);
+        }
+        });
     });
-});
-comments.addEventListener("input", () => {
-    let remaining = 500 - comments.value.length;
-    counter.textContent = `${remaining} characters remaining`;
-    if (remaining <= 50) {
-          if (remaining <= 0) {
-            if(remaining < 0) {
-                formerrors["comments"].push("Length exceeded");
+    comments.addEventListener("input", () => {
+        let remaining = 500 - comments.value.length;
+        counter.textContent = `${remaining} characters remaining`;
+        if (remaining <= 50) {
+              if (remaining <= 0) {
+                if(remaining < 0) {
+                    formerrors["comments"].push("Length exceeded");
+                }
+                if (counter.classList.contains("warn")) {
+                    counter.classList.remove("warn");
+                }
+                counter.classList.add("error");
+            } else {
+                if (counter.classList.contains("error")) {
+                    counter.classList.remove("error");
+                }
+                counter.classList.add("warn");
             }
+        } else {
             if (counter.classList.contains("warn")) {
                 counter.classList.remove("warn");
             }
-            counter.classList.add("error");
-        } else {
             if (counter.classList.contains("error")) {
                 counter.classList.remove("error");
             }
-            counter.classList.add("warn");
         }
-    } else {
-        if (counter.classList.contains("warn")) {
-            counter.classList.remove("warn");
-        }
-        if (counter.classList.contains("error")) {
-            counter.classList.remove("error");
-        }
-    }
-});
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    document.getElementById("form-errors").value = JSON.stringify(formerrors);
+    });
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        document.getElementById("form-errors").value = JSON.stringify(formerrors);
+    });
 });
