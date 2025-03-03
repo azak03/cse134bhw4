93f8e0f2a1a3fa049999f6a1947df822e5e2f546
azak03 <97641168+azak03@users.noreply.github.com>
2025-03-02 22:47:02 -0800

    Update theme.js

--- resources/theme.js
+++ resources/theme.js
@@ -2,21 +2,20 @@ document.addEventListener('DOMContentLoaded', function() {
     let themetoggle = document.getElementById("theme-button");
     themetoggle.classList.remove("off");
     themetoggle.addEventListener("click", () => {
-        alert("t");
         if (localStorage.getItem("light") == 'true') {
             localStorage.setItem("light", 'false');
             document.body.classList.remove("light-mode");
-            themetoggle.textContent = "☀️";
+            themetoggle.textContent = "🌙";
         } else {
             localStorage.setItem("light", 'true');
             document.body.classList.add("light-mode");
-            themetoggle.textContent = "🌙";  
+            themetoggle.textContent = "☀️";  
         }
     });
     
     let savedtheme = localStorage.getItem("light");
     if (savedtheme === 'true') {
         document.body.classList.add("light-mode");
-        themetoggle.textContent = "🌙";
+        themetoggle.textContent = "☀️";
     }
 });
